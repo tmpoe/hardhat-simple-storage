fec9d13fcf6ab676b0409163c35af41126ece258
tmpITK <torok.mark.patrik@gmail.com>
2022-11-20 11:03:46 +0100

    Add automatic etherscen verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.17",
-};
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+}
